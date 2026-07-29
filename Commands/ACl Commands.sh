2cc4c50005d163be1e79b1c74b404636ee106b63
dukaku <duraikavikumar@aliceblueindia.com>
2026-07-29 15:02:47 +0530

    ACL commands updated with delete commands

--- Commands/ACl Commands.sh
+++ Commands/ACl Commands.sh
@@ -21,9 +21,17 @@ setfacl -x u:username /path/to/file_or_directory
 
 setfacl -x g:groupname /path/to/file_or_directory
 
-# To remove a specific user recursively:
+# To remove default permissions for a user or group:
+
+setfacl -x d:u:username /path/to/file_or_directory
+
+setfacl -x d:g:groupname /path/to/file_or_directory
+
+# To remove the acl for a specific user recursively both current and default:
 
 setfacl -R -x u:username /path/to/directory
+
+setfacl -R -x d:u:username /path/to/directory
 
 # To completely wipe ALL ACL permissions (returning the file to standard chmod permissions):
 
@@ -48,3 +56,63 @@ setfacl -R -b /path/to/directory
 # To completely clean the directory so it stops applying those rules to new files, add the -k flag:
 
 setfacl -R -b -k /path/to/directory
+
+
+The most common uses are:
+
+1. Add or modify multiple ACL entries (-m)
+
+Instead of:
+
+setfacl -m u:alice:rwx /dir
+setfacl -m g:dev:r-x /dir
+setfacl -m d:u:alice:rwx /dir
+setfacl -m d:g:dev:r-x /dir
+
+You can do:
+
+setfacl -m u:alice:rwx,g:dev:r-x,d:u:alice:rwx,d:g:dev:r-x /dir
+2. Remove multiple ACL entries (-x)
+setfacl -x u:alice,g:dev,d:u:alice,d:g:dev /dir
+3. Combine with recursion (-R)
+setfacl -R -m u:alice:r-x,g:dev:r-x /data
+
+or
+
+setfacl -R -x u:alice,g:dev,d:u:alice,d:g:dev /data
+4. Mix users, groups, masks, and others
+
+You can specify several ACL entry types in one command:
+
+setfacl -m u:alice:rwx,g:dev:r-x,m:r-x,o:--- /dir
+
+This sets:
+
+u:alice:rwx
+g:dev:r-x
+mask::r-x
+other::---
+
+all in one operation.
+
+5. Default ACLs
+
+You can also set multiple default ACLs together:
+
+setfacl -m d:u:alice:rwx,d:g:dev:r-x,d:o:--- /dir
+General rule
+
+Any option that accepts an ACL specification (-m or -x) accepts a comma-separated list of ACL entries.
+
+Examples:
+
+# Multiple users
+setfacl -m u:alice:rwx,u:bob:r-x /dir
+
+# Multiple groups
+setfacl -m g:dev:rwx,g:qa:r-x /dir
+
+# Mix everything
+setfacl -m u:alice:rwx,g:dev:r-x,d:u:alice:rwx,d:g:dev:r-x,m:rwx /dir
+
+This is the preferred way to manage ACLs because it updates all the specified entries in a single invocation instead of running setfacl repeatedly.
